54accde48acd85859641ab6056754b15698ae381
Potato <67771312+sonsinger12350@users.noreply.github.com>
2024-12-01 15:18:17 +0700

    Delete wp-content/themes__ directory

--- wp-content/themes__/site/woocommerce/myaccount/my-point.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$get_points = (int) get_user_meta( get_current_user_ID(), 'wps_wpr_points', true );
-
-?>
-<h2 class="mb-4 section-header border-0 mg-top-0"><span>Điểm tích lũy</span></h2>
-<div class="my-points">
-    Điểm: <?php echo $get_points;?>
-</div>
